bc1ff3acef1444342e644aa9658b0269792dae82
Weronika K <weronika.kolodziejak@gmail.com>
2024-08-06 18:50:12 +0200

    feat: remove sui specific code

--- examples/nextjs-starter/app/layout.tsx
+++ examples/nextjs-starter/app/layout.tsx
@@ -3,17 +3,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { EnokiFlowProvider } from "@mysten/enoki/react";
-import { createNetworkConfig, SuiClientProvider } from "@mysten/dapp-kit";
-import { getFullnodeUrl } from "@mysten/sui/client";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
-
-// Config options for the networks you want to connect to
-const { networkConfig } = createNetworkConfig({
-  devnet: { url: getFullnodeUrl("devnet") },
-});
 
 export default function RootLayout({
   children,
@@ -22,13 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <SuiClientProvider networks={networkConfig} defaultNetwork="devnet">
         <EnokiFlowProvider apiKey={process.env.ENOKI_PUB_KEY!}>
           <body className={inter.className}>{children}</body>
           <Analytics />
           <Toaster closeButton  />
         </EnokiFlowProvider>
-      </SuiClientProvider>
     </html>
   );
 }
