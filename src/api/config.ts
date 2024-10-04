46e7058fe08ac383002c35231f7b927d4589e90e
Michal Kaliszewski <michal.kaliszewski00@gmail.com>
2024-10-04 10:08:07 +0200

    feat: use akrd.io

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,19 +2,19 @@ export const apiConfig = (env: string) => {
   switch (env) {
     case "mainnet":
       return {
-        apiUrl: "https://api.carmella.io",
-        cdnUrl: "https://cdn.carmella.io",
+        apiUrl: "https://api.akrd.io",
+        cdnUrl: "https://cdn.akrd.io",
       };
     case "testnet":
     default:
       return {
-        apiUrl: "https://dev-api.carmella.io",
-        cdnUrl: "https://dev-cdn.carmella.io",
+        apiUrl: "https://dev-api.akrd.io",
+        cdnUrl: "https://dev-cdn.akrd.io",
       };
     case "local":
       return {
         apiUrl: "http://localhost:3001",
-        cdnUrl: "http://localhost:3001/cached"
+        cdnUrl: "http://localhost:3001/data"
       };
   }
 };
