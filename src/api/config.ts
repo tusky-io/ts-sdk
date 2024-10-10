128f8d725141e1dea7eca5178cbf5e4e9cf37f54
Michal Kaliszewski <michal.kaliszewski00@gmail.com>
2024-10-10 18:36:59 +0200

    feat: use tusky.io

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,24 +2,24 @@ export const apiConfig = (env: string) => {
   switch (env) {
     case "mainnet":
       return {
-        apiUrl: "https://api.akrd.io",
-        cdnUrl: "https://cdn.akrd.io",
+        apiUrl: "https://api.tusky.io",
+        cdnUrl: "https://cdn.tusky.io",
       };
     case "testnet":
     default:
       return {
-        apiUrl: "https://dev-api.akrd.io",
-        cdnUrl: "https://dev-cdn.akrd.io",
+        apiUrl: "https://dev-api.tusky.io",
+        cdnUrl: "https://dev-cdn.tusky.io",
       };
     case "local":
       return {
         apiUrl: "http://localhost:3001",
-        cdnUrl: "http://localhost:3001/data"
+        cdnUrl: "http://localhost:3001/data",
       };
   }
 };
 
 export interface ApiConfig {
   apiUrl: string,
-  cdnUrl: string
+  cdnUrl: string,
 }
