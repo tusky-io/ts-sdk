1b15375a1fcc663ec9e06b5c88d491ef6870311c
Michal Kaliszewski <michal.kaliszewski00@gmail.com>
2024-11-15 17:31:37 +0100

    feat(payment): set 1 as default quantity

--- src/types/payment.ts
+++ src/types/payment.ts
@@ -14,7 +14,7 @@ export type PaymentSessionOptions = {
 }
 
 export type PaymentSessionPriceLookupKey = 'monthly' | 'yearly'
-export type PaymentSessionQuantity = 1000 | 2000
+export type PaymentSessionQuantity = 1 | 2 | 5 | 10
 
 
 export type PaymentPlan = {
