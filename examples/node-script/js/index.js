ee41636a88992c0b63eec9d08fde812cbe81a5bc
Weronika K <weronika.kolodziejak@gmail.com>
2025-04-23 16:21:08 +0200

    chore: adjust node script examples

--- examples/node-script/js/index.js
+++ examples/node-script/js/index.js
@@ -1,8 +1,9 @@
-import { Tusky } from "@tusky-io/ts-sdk";
+// TODO: make it work with import { Tusky } from "@tusky-io/ts-sdk";
+
+const { Tusky } = require("@tusky-io/ts-sdk");
 
 const main = async () => {
   const tusky = await Tusky.init({ apiKey: process.env.TUSKY_API_KEY });
-
 }
 
 main().catch(console.error);
