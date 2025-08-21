6a2782c8fd26c19b6c2f6724430258d85c76715b
Weronika K <weronika.kolodziejak@gmail.com>
2025-08-21 14:02:24 +0200

    feat: temporarily use react native import

--- src/crypto/libsodium.ts
+++ src/crypto/libsodium.ts
@@ -3,22 +3,25 @@ let sodium: any;
 export async function loadSodium(): Promise<any> {
   if (sodium) return sodium;
 
-  const runtime = getRuntime();
+  // const runtime = getRuntime();
 
-  if (runtime === "node") {
-    const module = await import("../platform/node");
-    sodium = (<any>module.default).default;
-    await sodium.ready;
-  } else if (runtime === "browser") {
-    const module = await import("../platform/browser");
-    sodium = (<any>module.default).default;
-    await sodium.ready;
-  } else if (runtime === "react-native") {
-    const module = await import("../platform/react-native");
-    sodium = module.default;
-  } else {
-    throw new Error("Unsupported runtime environment");
-  }
+  const module = await import("../platform/react-native");
+  sodium = module.default;
+
+  // if (runtime === "node") {
+  //   const module = await import("../platform/node");
+  //   sodium = (<any>module.default).default;
+  //   await sodium.ready;
+  // } else if (runtime === "browser") {
+  //   const module = await import("../platform/browser");
+  //   sodium = (<any>module.default).default;
+  //   await sodium.ready;
+  // } else if (runtime === "react-native") {
+  //   const module = await import("../platform/react-native");
+  //   sodium = module.default;
+  // } else {
+  //   throw new Error("Unsupported runtime environment");
+  // }
 
   return sodium;
 }
